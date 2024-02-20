f609d9be1dc2bf109d78434e40e1580c1717f519
MaksymP <maximus-ms@ukr.net>
2024-02-20 04:50:02 +0200

    PEP8 fix

--- KochSnowflake.py
+++ KochSnowflake.py
@@ -1,15 +1,16 @@
 import sys
 import turtle
 
-DEFAULT_N    = 3
+DEFAULT_N = 3
 DEFAULT_SIZE = 300
 
-def koch_snowflake(n=DEFAULT_N, l=DEFAULT_SIZE, funny:bool=False):
+
+def koch_snowflake(n=DEFAULT_N, l=DEFAULT_SIZE, funny: bool = False):
     def koch_curve(t, l, n):
         if n:
             for angle in [0, 60, -120, 60]:
                 t.right(angle)
-                koch_curve(t, l/3, n-1)
+                koch_curve(t, l / 3, n - 1)
             return
         t.forward(l)
 
@@ -17,16 +18,17 @@ def koch_snowflake(n=DEFAULT_N, l=DEFAULT_SIZE, funny:bool=False):
     t.clear()
     t.speed(10)
     t.shape("classic")
-    t.color('#285078')
+    t.color("#285078")
     t.penup()
-    t.goto(0, -l/2)
+    t.goto(0, -l / 2)
     t.pendown()
     t.left(60)
     while n >= 0:
         for _ in range(3):
             koch_curve(t, l, n)
             t.left(120)
-        if not funny: break
+        if not funny:
+            break
         n -= 1
 
 
